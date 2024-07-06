24b763de14b1a7e054fcb2bd94575dfb18c9eb9a
balaji-m <bmeenakshisundaram@cloudera.com>
2024-07-06 15:19:39 +0530

    Setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Stock market Chatbot',
+    version= '0.0.0',
+    author= 'Balaji M',
+    author_email= 'mbalaji131224@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
